a21bd44fb8c8dfaead5a16db43eab34304c8da2b
hyosupsong25 <hyosupsong25@gmail.com>
2021-03-30 23:18:32 +0900

    [21-03-30] Update and Delete function

--- src/main/java/com/song/service/springboot/service/posts/PostsService.java
+++ src/main/java/com/song/service/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.song.service.springboot.service.posts;
 
 import com.song.service.springboot.domain.posts.Posts;
 import com.song.service.springboot.domain.posts.PostsRepository;
+import com.song.service.springboot.web.dto.PostsListResponseDto;
 import com.song.service.springboot.web.dto.PostsResponseDto;
 import com.song.service.springboot.web.dto.PostsSaveRequestDto;
 import com.song.service.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Spring에서 Bean을 주입받는 방식들이 3가지 있다.
@@ -36,6 +40,17 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById(Long id) {
--- src/main/java/com/song/service/springboot/web/IndexController.java
+++ src/main/java/com/song/service/springboot/web/IndexController.java
@@ -1,9 +1,12 @@
 package com.song.service.springboot.web;
 
+import com.song.service.springboot.service.posts.PostsService;
+import com.song.service.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Mustache 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정된다.
@@ -12,18 +15,31 @@ import org.springframework.web.bind.annotation.GetMapping;
  * 즉, 여기서는 index를 반환하므로, src/main/resources/templates/index.mustache 로 전환되어 View Resolver가 처리하게 된다.
  *
  * ViewResolver는 URL 요청의 결과를 전달할 타입과 값을 지정하는 관리자 격
+ *
+ * Model
+ * 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장
  */
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
